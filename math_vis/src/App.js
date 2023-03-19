d03cef83285ebe17eccb39097a61496560430611
nickjg1 <ngyorgypal@gmail.com>
2023-03-18 22:20:46 -0400

    Created Prototype 6 (barebones home page)

--- math_vis/src/App.js
+++ math_vis/src/App.js
@@ -1,19 +1,15 @@
-import './styles/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComp from "./components/NavbarComp";
+import Lesson1 from "./components/pages/Lesson1";
 
 function App() {
-  return (
-    <div className="App">
-      <NavbarComp/>
-      <div className="container">
-        <button className="btn btn-primary d-block mx-auto my-5">Click me</button>
-      </div>
-      <div className="container">
-        <embed className="overflow-hidden d-block w-100" scrolling="no" src="elm/index.html" height="1200px"></embed>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavbarComp />
+			<Lesson1 />
+		</div>
+	);
 }
 
 export default App;
--- math_vis/src/components/NavbarComp.js
+++ math_vis/src/components/NavbarComp.js
@@ -1,25 +1,25 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function NavScrollExample() {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
+	return (
+		<Navbar bg="light" expand="lg">
+			<Container fluid>
+				<Navbar.Brand href="#">Learn Linear Algebra</Navbar.Brand>
+				<Navbar.Toggle aria-controls="navbarScroll" />
+				<Navbar.Collapse id="navbarScroll">
+					<Nav
+						className="me-auto my-2 my-lg-0"
+						style={{ maxHeight: "100px" }}
+						navbarScroll
+					>
+						<Nav.Link href="#action1">Home</Nav.Link>
+						<Nav.Link href="#action2">Academy</Nav.Link>
+						{/* <NavDropdown title="" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
                 Another action
@@ -28,24 +28,24 @@ function NavScrollExample() {
               <NavDropdown.Item href="#action5">
                 Something else here
               </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+            </NavDropdown> */}
+						<Nav.Link href="#" disabled>
+							Sandbox
+						</Nav.Link>
+					</Nav>
+					<Form className="d-flex">
+						<Form.Control
+							type="search"
+							placeholder="Search"
+							className="me-2"
+							aria-label="Search"
+						/>
+						<Button variant="outline-success">Search</Button>
+					</Form>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
 
 export default NavScrollExample;
