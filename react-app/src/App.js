7a8acf535fa02be331d1bfcb406b9c3cf8ba66d9
nickjg1 <ngyorgypal@gmail.com>
2023-03-24 15:07:27 -0400

    Added home route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,7 @@ const App = () => (
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" exact element={<Home />} />
+			<Route path="/home" exact element={<Home />} />
 			<Route path="/lessons" element={<Lessons />} />
 			<Route path="/lessons/:lessonTitle" element={<LessonPage />} />
 		</Routes>
