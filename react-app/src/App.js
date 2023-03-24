b79707853491cb851c91009275c20af41a95b062
nickjg1 <ngyorgypal@gmail.com>
2023-03-24 15:44:33 -0400

    GH pages stuff (still having issues)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,8 +4,8 @@ import { Home, LessonPage, Lessons } from './components';
 const App = () => (
 	<BrowserRouter>
 		<Routes>
-			<Route path="/" exact element={<Home />} />
-			<Route path="/home" exact element={<Home />} />
+			<Route path="/" element={<Home />} />
+			<Route path="/home" element={<Home />} />
 			<Route path="/lessons" element={<Lessons />} />
 			<Route path="/lessons/:lessonTitle" element={<LessonPage />} />
 		</Routes>
