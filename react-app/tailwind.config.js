1e899ebc42e3401a0326383483be2612cfcecdaa
NathanHartHDCH <n.bernard.hart@gmail.com>
2023-03-30 13:39:37 -0400

    Added new offblack

--- react-app/tailwind.config.js
+++ react-app/tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	mode: 'jit',
-	theme: {
-		extend: {
-			colors: {
-				primaryDark: '#322F5C',
-				primaryLight: '#0078FF',
-				secondLight: '#00E790',
-				offBlack: '#262030',
-				secondLight: '#2F525C',
-				highlight: '#2F525C',
-				secondaryHighlight: '#FBD887',
-				offWhite: '#FFFAEC',
-			},
-			fontFamily: {
-				sans: ['Assistant', 'sans-serif'],
-			},
-		},
-	},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primaryDark: "#322F5C",
+        primaryLight: "#0078FF",
+        secondLight: "#00E790",
+        offBlack: "#150F1E",
+		offBlack2: "#262030",
+        secondLight: "#2F525C",
+        highlight: "#2F525C",
+        secondaryHighlight: "#FBD887",
+        offWhite: "#FFFAEC",
+      },
+      fontFamily: {
+        sans: ["Assistant", "sans-serif"],
+      },
+    },
+  },
 };
