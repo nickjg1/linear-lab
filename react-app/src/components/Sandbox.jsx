970b3d3c41051958d3690456146610fa7a6ade6f
NathanHartHDCH <n.bernard.hart@gmail.com>
2023-03-30 19:10:16 -0400

    Added link to sandbox for UI prototype

--- react-app/src/components/Sandbox.jsx
+++ react-app/src/components/Sandbox.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Sandbox = () => {
-	return (
-		<div>
-			<embed
-				className="flex flex-col h-[90.65vh] w-full"
-				src={`${process.env.PUBLIC_URL}/elm/index.html`}
-			></embed>
-		</div>
-	);
+    return (
+        <div className='relative'>
+            <a href='#'>
+                <div className='absolute w-[4rem] h-[4rem] bg-offBlack2 top-5 right-5 rounded-full flex items-center hover:bg-primaryDark'>
+                    <i class='fa-solid fa-image mx-auto text-[2rem]'></i>
+                </div>
+            </a>
+            <embed
+                className='flex flex-col h-[93vh] lg:h-[92vh] w-full'
+                src={`${process.env.PUBLIC_URL}/elm/index.html`}
+            ></embed>
+        </div>
+    );
 };
 
 export default Sandbox;
